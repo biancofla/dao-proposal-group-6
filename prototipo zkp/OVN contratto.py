c420ad1ab84c9c93012255f257673ff647f7a8c7
r-marche <91833417+r-marche@users.noreply.github.com>
2022-10-15 12:36:36 +0200

    Add files via upload

--- prototipo zkp/OVN contratto.py
+++ prototipo zkp/OVN contratto.py
@@ -53,20 +53,29 @@ def pow_mod(base, exp, mod):
         print(res)
     return mult_mod(res,base, mod)
 
-def euclid(a, b):
-    s1, s2 = 1, 0
+def inverso_mod(b,mod):
+    a= mod
     t1, t2 = 0, 1
+    segnoT1, segnoT2 = 1, 1
     while b != 0:
-        quotient, remainder = a/b, a%b
+        quotient, remainder = a//b, a%b
         a = b
         b = remainder
-        s1, s2 = s2, s1 - quotient * s2
-        t1, t2 = t2, t1 - quotient * t2
-    return a, s1, t1
-
-def inverso_mod(a, mod):
-    _, _, res = euclid(mod, a)
-    return res % mod
+        tmp, segnoTmp = t2, segnoT2
+        if segnoT1 == segnoT2:
+            if t1 >= quotient * t2:
+                t2 = t1 - quotient * t2
+            else:
+                t2 = quotient * t2 - t1
+                segnoT2 = segnoT2 ^ 1  # xor per cambiare segno
+        else:
+            t2 = t1 + quotient * t2
+            segnoT2 = segnoT2 ^ 1
+        t1, segnoT1 = tmp, segnoTmp
+    if segnoT1 == 0:
+        return mod-t1
+    else:
+        return t1
 
 """Funzione chiamata con i parametri passati dal partecipante
    Verifica che il partecipante i-esimo effettivamente conosce un segreto x_i
@@ -81,16 +90,27 @@ def verifyR1(i,claimId, claimRnd, proof):
 
 def verifyR2(i,commit, key, voto, a1, b1, a2, b2, d1, r1, d2, r2):
     chal = int(hashlib.sha256(str([i,commit, key, voto, a1, b1, a2, b2]).encode('utf-8')).hexdigest(), 16) % q
-    check1 = (chal == ((d1 + d2) % q))
-    check2 = (a1 == ((pow(gen, r1, p) * pow(commit, d1, p)) % p))
-    check3 = (a2 == ((pow(gen, r2, p) * pow(commit, d2, p)) % p))
-    # print(a2, pow(g,r2, p), pow(commit,d2, p), ( (pow(g,r2, p) * pow(commit, d2, p)) %p) )
-
-    check4 = (b1 == ((pow(key, r1, p) * pow(voto, d1, p)) % p))
-    check5 = (b2 == ((pow(key, r2, p) * pow(voto, d2, p) * pow(inverso_mod(p, gen), d2, p)) % p))
+    check1 = (chal == add_mod(d1 ,d2, q) )
+    check2 = (a1 == mult_mod( pow_mod(gen, r1, p) , pow_mod(commit, d1, p) , p) )
+    check3 = (a2 == mult_mod(pow_mod(gen, r2, p), pow_mod(commit, d2, p),p))
+    check4 = (b1 == mult_mod(pow_mod(key, r1, p), pow_mod(voto, d1, p), p))
+    check5 = (b2 == mult_mod(pow_mod(key, r2, p) , pow_mod(mult_mod(voto,inverso_mod(gen,p),p), d2, p) , p))
     print(check1, check2, check3, check4, check5)
     if (check1 and check2 and check3 and check4 and check5):
         voti[i] = voto
         return True
     else:
         return False
+
+def conta_voti():
+    tot =1
+    for i in range(0,n):
+        tot = mult_mod(tot, voti[i],p)
+
+    testTot = 1
+    if (testTot == tot):
+        return 0
+    for j in range(0,n):
+        testTot = mult_mod(testTot, gen, p)
+        if testTot == tot:
+            return j+1
